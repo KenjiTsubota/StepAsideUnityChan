2b028a1f6f1ba38a231fb4a9db568bf24347c563
Kenji Tsubota <kenji.tsubota@scsk.jp>
2019-07-04 14:30:45 +0900

    Lesson6課題
    Destroy処理をOnBecameInvisible関数を使わないよう修正

--- Assets/ItemDestroyer.cs
+++ Assets/ItemDestroyer.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemDestroyer : MonoBehaviour {
+	//カメラのオブジェクト
+	private GameObject camera;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		//カメラのオブジェクトを取得
+		this.camera = GameObject.Find ("Main Camera");
+
 	}
 
+	// Update is called once per frame
+	void Update () {
+		//カメラの位置より後ろであればオブジェクトを破棄する
+		if (this.transform.position.z < camera.transform.position.z) {
+
+			//オブジェクトを破棄する
+			Destroy (this.gameObject);
+		}
+
+	}
+/*
 	// オブジェクトが画面外に出た場合の処理
 	void OnBecameInvisible(){
 
@@ -21,4 +31,5 @@ public class ItemDestroyer : MonoBehaviour {
 		Destroy(this.gameObject);
 
 	}
+*/
 }
